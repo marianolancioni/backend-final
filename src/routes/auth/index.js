5925eb9f7ab059b11801123fd2c475cfd49936f5
Mariano David Lancioni <mdl@mutual.local>
2021-08-12 13:05:52 -0300

    correccion final

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -11,8 +11,8 @@ router.route('/login')
       if (!req.body.password || typeof req.body.password !== 'string') {
         return res.status(400).send({message: 'Password invalido'});
       }
-    //const md5_password = crypto.createHash('md5').update(req.body.password).digest("hex");
-    UsuarioSchema.findOne({email:req.body.email, password: req.body.password}).then((usuario) => {
+    const md5_password = crypto.createHash('md5').update(req.body.password).digest("hex");
+    UsuarioSchema.findOne({email:req.body.email, password: md5_password}).then((usuario) => {
         if (usuario) {
             res.status(200).send(usuario);
         } else {
